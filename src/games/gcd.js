8188623a2a67aa2001fe4d399189982112b07275
Mihail Ogarkov <medvedrulit2021@gmail.com>
2021-10-23 18:22:49 +0300

    gcd

---
+++ src/games/gcd.js
@@ -0,0 +1,25 @@
+import getRandomInt from "../utilities.js";
+import startNewGame from "../index.js";
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+const calculateGCD = (firstNumber, secondNumber) => {
+  if (secondNumber === 0) {
+    return firstNumber;
+  }
+  return calculateGCD(secondNumber, firstNumber % secondNumber);
+};
+
+const generateQuestionAnswer = () => {
+  const firstNumber = getRandomInt(0, 100);
+  const secondNumber = getRandomInt(0, 100);
+
+  const question = `${firstNumber} ${secondNumber}`;
+  const answer = calculateGCD(firstNumber, secondNumber).toString();
+
+  return [question, answer];
+};
+
+const gcdBrainGames = () => startNewGame(description, generateQuestionAnswer);
+
+export default gcdBrainGames;
